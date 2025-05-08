0db73218295c756a33e20d24682e03876805969c
Hager <hagertaher257@gmail.com>
2025-05-08 07:38:07 +0300

    last

---
+++ SWE2-Project-master/Backend/controllers/auth.controller.js
@@ -0,0 +1,94 @@
+const bcrypt = require("bcrypt");
+const Users = require("../models/user");
+
+const register = async (req, res) => {
+    const {
+        firstName,
+        lastName,
+        mobile,
+        gender,
+        username,
+        email,
+        password,
+        confirmPassword,
+        isAdmin,
+        isTest,
+    } = req.body;
+
+    if (!firstName || !lastName || !mobile || !gender || !username || !email || !password || !confirmPassword) {
+        return res.status(400).json({
+            status: "failure",
+            message: "Missing required fields",
+        });
+    }
+
+    if (password !== confirmPassword) {
+        return res.status(400).json({
+            status: "failure",
+            message: "Passwords do not match",
+        });
+    }
+
+    const existingUser = await Users.findOne({ email: email });
+
+    if (existingUser) {
+        return res.status(400).json({
+            status: "failure",
+            message: "User already exists",
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new Users({
+        firstName: firstName,
+        lastName: lastName,
+        mobile: mobile,
+        gender: gender,
+        username: username,
+        email: email,
+        password: hashedPassword,
+        confirmPassword: confirmPassword,
+        isAdmin: isAdmin,
+        isTest: isTest,
+    });
+
+    await newUser.save();
+    req.session.user = newUser;
+    res.redirect("/home");
+};
+
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await Users.findOne({ email: email });
+    // console.log(user.username);
+    if (!user) {
+        return res.status(400).json({
+            status: "failure",
+            message: "Invalid email or password",
+        });
+    }
+
+    const correctPassword = await bcrypt.compare(password, user.password);
+    
+    if (!correctPassword) {
+        return res.status(400).json({
+            status: "failure",
+            message: "Invalid email or password",
+        });
+    }
+
+    req.session.user = user;
+    res.redirect("/home");
+}
+
+const logout = async (req, res) => {
+    req.session.destroy();
+    res.redirect("/login");
+}
+
+module.exports = {
+    register,
+    login,
+    logout,
+}
