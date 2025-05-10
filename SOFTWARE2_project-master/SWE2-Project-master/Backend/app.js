638ffa44cfc3cdf9da4faafefcbd2c5a388ed135
Hager <hagertaher257@gmail.com>
2025-05-10 23:36:54 +0300

    last ISA

--- SOFTWARE2_project-master/SWE2-Project-master/Backend/app.js
+++ SOFTWARE2_project-master/SWE2-Project-master/Backend/app.js
@@ -105,7 +105,7 @@ app.use('/', pageRoutes);
 
 // app.use('/', authRoutes);
 
-// app.use('/', pageRoutes);
+ app.use('/', pageRoutes);
 const isAuthenticated = require('./middlewares/authenticate');
 app.get('/protected', isAuthenticated, (req, res) => {
     res.status(200).json({ message: 'Access granted' });
